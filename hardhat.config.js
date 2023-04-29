89b2e1063a7c80959f5729fbf3a441d69783ec81
KerimGurbaz <101603320+KerimGurbaz@users.noreply.github.com>
2023-04-29 23:34:02 +0200

    test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {},
+  },
+  etherscan: {
+    apiKey: "6X18FAVZEHZRHNWFTIVFDN7EXKQI64G7MC",
+  },
 };
